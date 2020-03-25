0ca606f96fdd225efad2c92af4ee5772a2aed77f
Yamid Cueto <ycueto.extreme@gmail.com>
2020-03-25 10:42:37 -0500

    update changes

--- views/template.php
+++ views/template.php
@@ -9,34 +9,12 @@
     <title>Template</title>
 </head>
 <body>
-    <nav  class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-            <img src="./public/img/logo.png" alt="_logo" width="36" height="36" class="d-block"
-                 viewBox="0 0 612 612" role="img" focusable="false">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">About us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Contact us</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-    <section>
-        <h1>Home page</h1>
-    </section>
+    <?php require_once 'modules/navigation.php'; ?>
+    <div class="container">
+        <section>
+            <h1>Home page</h1>
+        </section>
+    </div>
 
     <script src="./public/js/jquery-3.4.1.slim.min.js"></script>
     <script src="./public/js/popper.min.js"></script>
