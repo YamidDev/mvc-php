85c3baaf02db9faae9c46a6b0be8c3cde937b5e3
Yamid Cueto <ycueto.extreme@gmail.com>
2020-03-25 10:05:52 -0500

    Initial commit

---
+++ controllers/controller.php
@@ -0,0 +1,8 @@
+<?php
+
+    class MvcController {
+
+        public function template() {
+            include 'views/template.php';
+        }
+    }
---
+++ views/template.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template</title>
+</head>
+<body>
+    <header>
+        <h1>LOGO</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About us</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">Contact us</a></li>
+        </ul>
+    </nav>
+    <section>
+        <h1>Home page</h1>
+    </section>
+</body>
+</html>
