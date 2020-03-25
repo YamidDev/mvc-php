86fc03c9c30d65b8bc20efc80a6db6192de9402d
Yamid Cueto <ycueto.extreme@gmail.com>
2020-03-25 10:33:00 -0500

    update changes, add jQuery, Bootstrap css and popper

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
 
+    require_once 'controllers/controller.php';
+
+    $mvc = new MvcController();
+    $mvc->template();
--- views/template.php
+++ views/template.php
@@ -5,23 +5,41 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./public/css/bootstrap.min.css">
     <title>Template</title>
 </head>
 <body>
-    <header>
-        <h1>LOGO</h1>
-    </header>
-
-    <nav>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About us</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Contact us</a></li>
-        </ul>
+    <nav  class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src="./public/img/logo.png" alt="_logo" width="36" height="36" class="d-block"
+                 viewBox="0 0 612 612" role="img" focusable="false">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">About us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="">Contact us</a>
+                </li>
+            </ul>
+        </div>
     </nav>
     <section>
         <h1>Home page</h1>
     </section>
+
+    <script src="./public/js/jquery-3.4.1.slim.min.js"></script>
+    <script src="./public/js/popper.min.js"></script>
+    <script src="./public/js/bootstrap.min.js"></script>
 </body>
 </html>
